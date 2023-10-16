04307eb17cacb63877bbd39772b346064c560f3e
Shansai Muraleetharan <smuraleetharan@kpmg.com>
2023-10-16 09:47:19 +0200

    AppSettings can be retrieved with EntraIdClient

---
+++ ADChecker/EntraIDClient.cs
@@ -0,0 +1,11 @@
+﻿namespace ADChecker;
+
+internal class EntraIDClient
+{
+    internal static List<EntraID> GetByUpn(string upn)
+    {
+        
+    }
+}
+
+internal record EntraID(string Upn, string Status);
